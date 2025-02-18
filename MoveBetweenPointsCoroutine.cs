b484b9f6fd6b18cf503db4cedee5e74b480b5475
Luca <57229449+Kizorat@users.noreply.github.com>
2025-02-18 10:30:09 +0100

    MoveBetweenPoint
    Creazione Script per oggetti di qualsiasi forma per il movimento da un punto A a B e viceversa

---
+++ MoveBetweenPointsCoroutine.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBetweenPointsCoroutine : MonoBehaviour
+{
+    public Vector3 pointA;
+    public Vector3 pointB;
+    public float duration = 2.0f;
+
+    void Start()
+    {
+        StartCoroutine(MoveObject());
+    }
+
+    IEnumerator MoveObject()
+    {
+        while (true)
+        {
+            yield return StartCoroutine(MoveToPosition(pointB));
+            yield return StartCoroutine(MoveToPosition(pointA));
+        }
+    }
+
+    IEnumerator MoveToPosition(Vector3 target)
+    {
+        Vector3 startPosition = transform.position;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < duration)
+        {
+            transform.position = Vector3.Lerp(startPosition, target, elapsedTime / duration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = target;
+    }
+}
