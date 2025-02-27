a49ce8c91f114d60bfd57f5a40e219d951451aa9
Grazia <g.dipietro4@studenti.unisa.it>
2025-02-27 14:34:41 +0100

    Semaforo ottimizzato
    Per garantire la gestione ottimizzata del semaforo è stato modificato lo script ed è stata inserita una singola luce che cambia colore.

---
+++ SemaforoSingolo.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using UnityEngine;
+
+public class SemaforoSingolo : MonoBehaviour {
+
+    public Light luce; //spotLight
+    public float tempoRosso = 8f;
+    public float tempoGiallo = 5f;
+    public float tempoVerde = 15f;
+
+    public enum StatoSemaforo { Rosso, Giallo, Verde } //possibili valori che può assumere il semaforo
+    public StatoSemaforo stato;
+
+    void Start() {
+        StartCoroutine(CicloSemaforo());
+    } //all'inizio del game parte il semaforo
+
+    //Gestione autonoma della luce
+    IEnumerator CicloSemaforo() {
+        while (true) {
+            stato = StatoSemaforo.Rosso;
+            luce.color = Color.red;  // Usare direttamente Color.red
+            yield return new WaitForSeconds(tempoRosso);
+            
+            stato = StatoSemaforo.Giallo;
+            luce.color = Color.yellow;  // Usare direttamente Color.yellow
+            yield return new WaitForSeconds(tempoGiallo);
+
+            stato = StatoSemaforo.Verde;
+            luce.color = Color.green;  // Usare direttamente Color.green
+            yield return new WaitForSeconds(tempoVerde);
+        }
+    }
+}
